66d819ba1929b76e86a4c777d3aa15fcfdb9786a
Andrew766938 <rnbrc122@gmail.com>
2025-12-16 01:56:33 +0300

    Update db_manager.py
    Add all missing repositories (posts, comments, likes, friendships) to DBManager to fix 500 errors in API endpoints

--- app/database/db_manager.py
+++ app/database/db_manager.py
@@ -1,6 +1,10 @@
 from app.database.database import async_session_maker
 from app.repositories.roles import RolesRepository
 from app.repositories.users import UsersRepository
+from app.repositories.posts import PostRepository
+from app.repositories.comments import CommentRepository
+from app.repositories.likes import LikeRepository
+from app.repositories.friendships import FriendshipRepository
 
 
 class DBManager:
@@ -9,10 +13,15 @@ class DBManager:
 
     async def __aenter__(self):
         self.session = self.session_factory()
-        # TODO Добавить сюда созданные репозитории
-        # Пример:
+        
+        # Initialize all repositories
         self.users = UsersRepository(self.session)
         self.roles = RolesRepository(self.session)
+        self.posts = PostRepository(self.session)
+        self.comments = CommentRepository(self.session)
+        self.likes = LikeRepository(self.session)
+        self.friendships = FriendshipRepository(self.session)
+        
         return self
 
     async def __aexit__(self, *args):
