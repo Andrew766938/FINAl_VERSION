d366eae5c2396e7625d6311929f0fc38ebc180eb
Andrew766938 <rnbrc122@gmail.com>
2025-12-18 06:26:31 +0300

    Удален неиспользуемый импорт Boolean

--- app/models/users.py
+++ app/models/users.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String, Float, ForeignKey, Boolean
+from sqlalchemy import String, Float, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database.database import Base
 
